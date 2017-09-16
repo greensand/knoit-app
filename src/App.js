ba6e98585ff82911323203c7db5b2f788375298d
nithinlatha <latha@appinessworld.com>
2017-09-16 18:20:06 +0530

    created menu screen

--- src/App.js
+++ src/App.js
@@ -8,12 +8,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-         <img src={logo} className="App-logo" alt="logo" />
-         <Menu/>
-           <Editprofile/>
-        </div>
-
+          <div className="App-header">
+           <img src={logo} className="App-logo" alt="logo" />
+           <Menu/>
+             <Editprofile/>
+          </div>
       </div>
     );
   }
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,25 +1,94 @@
 import React, { Component } from 'react';
-import { Col,Tabs,Tab,Grid,Row} from 'react-bootstrap';
-import splashscreen from '../images/splashscreen.png';
-
-
+import { Col,Tab,Grid,Row,Nav,NavItem} from 'react-bootstrap';
 
 export default class Menu extends Component {
-
+  constructor(props){
+    super(props);
+    this.state = {
+      key: 'first'
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+   handleSelect(key) {
+     this.setState({key:key});
+   }
   render() {
     return (
       <section className="chef-menu">
       <Grid>
       <Row>
        <div className="chef">
-       <Col lg={12} md={12} sm={12} xs={12} >
-          <h2>Menu</h2>
-          <Tabs>
-           <Tab eventKey={1} title="Tab 1">Tab 1 content</Tab>
-           <Tab eventKey={2} title="Tab 2">Tab 2 content</Tab>
-           <Tab eventKey={3} title="Tab 3" disabled>Tab 3 content</Tab>
-         </Tabs>
-       </Col>
+       <Tab.Container  id="left-tabs-example" defaultActiveKey={this.state.key} onSelect={this.handleSelect}>
+    <Row className="clearfix">
+    <Col sm={12}>
+
+    <Nav bsStyle="pills" >
+        <NavItem eventKey="first">
+          CHATS
+        </NavItem>
+        <NavItem eventKey="second">
+          MENU
+        </NavItem>
+        <NavItem eventKey="third">
+          BILLS
+        </NavItem>
+        <NavItem eventKey="fourth">
+          REVIEWS
+        </NavItem>
+        <NavItem eventKey="fifth">
+          ABOUT ME
+        </NavItem>
+    </Nav>
+      </Col>
+      <Col sm={6}>
+      <div className="content-holder">
+      <Tab.Content animation>
+        <Tab.Pane eventKey="first">
+      Chats
+        </Tab.Pane>
+        <Tab.Pane eventKey="second">
+        <div className="menu-holder">
+          <Col sm={6} className="pad-0">
+            <p>Todays Special</p>
+            <ul>
+              <li>Apple Crumble</li>
+              <li>Oreo Cheese Cake</li>
+              <li>Mango Milk Shake</li>
+            </ul>
+          </Col>
+          <Col sm={3} className="pad-0">
+            <p>Price</p>
+            <ul>
+              <li>250</li>
+              <li>150</li>
+              <li>180</li>
+            </ul>
+          </Col>
+          <Col sm={3} className="pad-0">
+            <p>Quantity</p>
+            <ul>
+              <li>7/17</li>
+              <li>10/20</li>
+              <li>5/6</li>
+            </ul>
+          </Col>
+        </div>
+        </Tab.Pane>
+        <Tab.Pane eventKey="third">
+          Bills
+        </Tab.Pane>
+        <Tab.Pane eventKey="fourth">
+          Reviews
+        </Tab.Pane>
+        <Tab.Pane eventKey="fifth">
+          About me
+        </Tab.Pane>
+      </Tab.Content>
+      </div>
+
+      </Col>
+    </Row>
+   </Tab.Container>
        </div>
       </Row>
       </Grid>
